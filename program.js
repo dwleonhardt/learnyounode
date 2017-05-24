66dc8ede155986a17239b2163dfca8e982fc92cd
dwleonhardt <dwleonhardt@gmail.com>
2017-05-24 09:55:07 -0600

    answer another

--- program.js
+++ program.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
-// return fs.readFileSync(process.argv[1]);
-fs.readFile(process.argv[2], function(err, stuff){
-  var string = stuff.toString().split('\n').length -1;
-  console.log(string);
+var path = require('path');
+ module.exports = function sort(dirname, file, callback) {
 
-});
+  // fs.readdir(process.argv[2], function(err, list){
+  //   list.forEach(function(ele){
+  //     if(path.extname(ele) === '.' +  process.argv[3]) {
+  //       console.log(ele);
+  //     }
+  //   });
+  // });
+}
